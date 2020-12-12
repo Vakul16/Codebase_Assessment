83e066e23d75a4ac213be99eb7a77313fc352e15
Vakul16 <valusavakhul@gmail.com>
2020-12-12 23:23:34 +0530

    modified js code

--- website/codebase.js
+++ website/codebase.js
@@ -1,3 +1,54 @@
+function removeColumn(thisID){
+  $('#'+thisID).css('background-color', 'blue');
+  // document.getElementById.style('background-color', 'blue');
+}
+
+  counter = 0 ;
+const imgs = document.querySelectorAll("img");
+  imgs.forEach(img => { 
+  
+  if(img.clientWidth >= 500 && img.clientHeight >= 150 && counter < 2){
+    console.log(img.src);
+    console.log("width"+img.clientWidth);
+    console.log("height"+img.clientHeight);
+    
+    var iDiv = document.createElement('div');
+      // iDiv.id = 'block'+counter;
+      iDiv.setAttribute("style","height:100%;width:100%;");
+      
+    var iframe = document.createElement('iframe');
+    iframe.id = 'block1'+counter;
+    iframe.setAttribute("onclick", "removeColumn(this.id)");
+    iframe.setAttribute("style","height:100%;width:100%;background-color:red");
+    iDiv.appendChild(iframe);	
+    img.after(iDiv);
+    counter = counter +1 ;
+  }
+  
+});
+// let ele = document.createElement('div')
+// ele.style.width = '728px';
+// ele.style.height = '90px';
+// document.body.appendChild(ele)
+// function prepareFrame() {
+//     //   let div1 = document.createElement('div')
+//     var ifrm = document.createElement('iframe')
+//     ifrm.setAttribute('img', {class : "img-fluid mr-4"});
+//     // ifrm.setAttribute('src', 'website\images\news\img-1.jpg');
+//     ifrm.style.backgroundColor = "red";
+//     ifrm.style.width = '728px'
+//     ifrm.style.height = '90px'
+//     ifrm.style.scrolling="no";
+//     var images = document.querySelectorAll("img");
+//     if(images === [1] && [2]) {
+//       var image = images;
+//       console.log(image.getAttribute('src'));
+//   }
+//   ele.appendChild(ifrm);
+// }
+// prepareFrame()
+
+
 // document.getElementById("entry-content");
 // document.getElementById("myParagraph").innerHTML = "This is <em>your</em> paragraph!";
 
@@ -13,44 +64,10 @@
 // or, if you want the unresolved src, as it appears in the original HTML:
 
 // var rawImgSrc = firstImage ? firstImage.getAttribute("src") : "";
-
-
 // <iframe allowtransparency="true" style="background: #FFFFFF;" 
 //     src="http://zingaya.com/widget/9d043c064dc241068881f045f9d8c151" 
 //     frameborder="0" height="184" width="100%">
 // </iframe>
-
-let ele = document.createElement('div')
-// ele.innerHTML = post_body;
-// var firstImage = ele.getElementsByTagName('img')[0]
-// var imgSrc = firstImage ? firstImage.src : "";
-// var rawImgSrc = firstImage ? firstImage.getAttribute("src") : "";
-// let text = document.createTextNode('something is showing')
-// ele.appendChild(text)
-ele.style.width = '728px';
-ele.style.height = '90px';
-// div.style.width = '728px';
-// div.style.height = '90px';
-document.body.appendChild(ele)
-// console.log(ele)  'file:///C:/Users/Sanjana/Downloads/SDE-Test/website/images/news/img-1.jpg'
-// document.getElementById("myP").style.backgroundColor = "red";
-
-
-
-function prepareFrame() {
-    //   let div1 = document.createElement('div')
-    var ifrm = document.createElement('iframe')
-    ifrm.setAttribute('img', {class : "img-fluid mr-4"});
-    // ifrm.setAttribute('src', 'website\images\news\img-1.jpg');
-    ifrm.style.backgroundColor = "red";
-    ifrm.style.width = '728px'
-    ifrm.style.height = '90px'
-    ifrm.style.scrolling="no";
-    var images = document.querySelectorAll("img");
-    if(images === [1] && [2]) {
-      var image = images;
-      console.log(image.getAttribute('src'));
-  }
     // images.onload = function(){
     //   var imageWidth = this.offsetWidth,
     //       imageHeight = this.offsetHeight,
@@ -63,12 +80,8 @@ function prepareFrame() {
     //                    window.pageYOffset + 'px';
     // }
     // ifrm.style.scrolling="overflow:hidden;
-    ele.appendChild(ifrm);
     // <iframe src="otherpage.html" scrolling="no" style="overflow:hidden;"></iframe>
     // document.body.appendChild(ifrm)
-  }
-  prepareFrame()
-
 // var image1 = document.querySelectorAll("img:nth-child(NodeList[1])");
 // console.log(image1);
 // var div = document.createElement('div');
